3a2181a15a8e3389ac6426b4b5687651f5a84e79
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-12-04 00:44:08 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,3 @@
+const fs= require('fs');
+console.log("hello samr");
+    fs.writeFileSync("hello.txt","Hello Samrat") // This is Create file which name is hello and write text inside the file "Hello Samrat"
---
+++ index.js
@@ -0,0 +1,2 @@
+const h1=require('h1').writeFileSync;
+h1("hellotc.txt","avf")
