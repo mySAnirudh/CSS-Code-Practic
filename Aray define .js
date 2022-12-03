074626ecc2de87f64e8c51ddaae95506fe2c4906
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-12-03 23:52:37 +0530

    Add files via upload

---
+++ Aray define .js
@@ -0,0 +1,6 @@
+var x= '20';
+const arr=[1,2,3,4,5,6];
+    console.log(arr);
+
+    // Cannot use in node import export as use in js
+    // Object ke under equal to nahi laga sakte hai 
---
+++ Filter match value.js
@@ -0,0 +1,10 @@
+const arr=[1,2,3,3,3,4,4,5,5,6,6,7,4,,2,2,1,,4,4,5,3,2,2,2,,3,,4,3,,4];
+
+let result=arr.filter((item)=>{
+    return item===3
+
+    //    return item>3 We can also here find the grather than value or any thing ike equal to 
+    
+})
+
+console.log(result)
